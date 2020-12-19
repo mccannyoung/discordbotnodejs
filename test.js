d19638699fdfe0a9a6dc080096f67460dfe6118d
R@MY <mccannyoung@users.noreply.github.com>
2020-12-18 20:22:38 -0600

    Fixed a bunch of stuff, will clean up after this commit

--- test.js
+++ test.js
@@ -39,7 +39,7 @@ client.on('ready', () => {
   timeZones["C"] = { offset:3, name: 'Charlie Time (Middle East)'};
   timeZones["D"] = { offset:4, name: 'Delta Time (Middle East)'};
 
-  channel.send('Discord bot ready');
+  //channel.send('Discord bot ready');
 });
 
 client.on('message', msg => {
@@ -54,36 +54,116 @@ client.on('message', msg => {
       msg.channel.send("there is no date");
     }
 
-    msg.channel.send(parseThis);
-    var hour = parseThis.substr(parseThis.indexOf(':')-2, parseThis.indexOf(':'));
-    msg.channel.send(hour);
-    var minutes = parseThis.substr(parseThis.indexOf(':')+1, 2);
-    msg.channel.send(minutes);
-    var amPM = parseThis.includes("AM")? "AM": "PM"
-    msg.channel.send(amPM);
-    var timeZone = parseThis.substr(parseThis.length-4, 4).trim();
-    msg.channel.send(timeZone);
-    if(amPM === 'PM' && hour !== '12') {
-      hour = hour + 12;
-      msg.channel.send(hour);
+    // msg.channel.send(parseThis);
+    // var  = parseThis.substr(0, parseThis.indexOf(':'));
+    // msg.channel.send("hour" + hour);
+    // var minutes = parseThis.substr(parseThis.indexOf(':')+1, 2);
+    // msg.channel.send("minutes" + minutes);
+    // var amPM = parseThis.includes("AM")? "AM": "PM"
+    // msg.channel.send(amPM);
+    //var timeZone = parseThis.substr(parseThis.length-4, 4).trim();
+    var timeZone = findTimeZone(parseThis);
+    //console.log("returned TZ = "+ timeZone);
+    //msg.channel.send("What timezone? " + timeZone);
+    // if(amPM === 'PM' && hour !== '12') {
+    //   hour = hour + 12;
+    //   msg.channel.send("new hour" + hour);
+    // }
+    parseThis = parseThis.replace(timeZone, '').trim();
+    //console.log("time without timezone: " + parseThis);
+
+    // var d = new Date();
+    // var time = parseThis.match( /(\d+)(?::(\d\d))?\s*(P?)/ );
+    // console.log("time array: "+ time.toString());
+    // console.log(`${time[1]} + (${time[3]} ? 12:0)`);
+    // d.setHours( parseInt( time[1], 10) + (time[3] ? 12 : 0) );
+    // console.log("hours? " + d.getHours());
+    // d.setMinutes( parseInt( time[2]) || 0 );
+    // console.log("minutes? " + d.getMinutes());
+    var inputTime = parseTime(parseThis);
+    //var inputTime = d;
+    //console.log("date object made " + inputTime);
+    //msg.channel.send(inputTime);
+    var time = convertTime2UTC(inputTime.getHours(),inputTime.getMinutes(),timeZone);
+    msg.channel.send("UTC time: "+ time);
+    var currentTime = new Date();
+    var seconds = (time.getTime() - currentTime.getTime()) / 1000;
+    console.log(" the difference is " + seconds);
+    
+    msg.channel.send("It will be that time in "+ seconds.toString());
+    msg.channel.send("let's see how this looks " + new Date(seconds * 1000).toISOString().substr(11, 8))
+    rHours = Math.floor(seconds / 3600);
+    seconds %= 3600;
+    rMinutes = Math.floor(seconds / 60);
+    rSeconds = Math.round(seconds % 60);
+    msg.channel.send("or " + rHours + " hours "+ rMinutes + " minutes and "+ rSeconds + " seconds");
+    if (seconds < 0){
+      seconds = Math.abs(seconds);
+      rHours = Math.floor(seconds / 3600);
+    seconds %= 3600;
+    rMinutes = Math.floor(seconds / 60);
+    rSeconds = Math.round(seconds % 60);
+    msg.channel.send("or " + rHours + " hours "+ rMinutes + " minutes and "+ rSeconds + " seconds ago");
     }
-    var time = convertTime2UTC(hour+":"+minutes+ timeZones);
-    var howLong = time = Date.now();
-    msg.channel.send(howLong);
   } else if (msg.content == `${prefix}help`){
     msg.channel.send("Hello friend!");
     msg.channel.send(`To allow me to better assist you please use "${prefix}when " followed by the time you want to know how long until`);
-    msg.channel.send('Here is a list of the available timezones '+ printTimeZones());
+    msg.channel.send('For example if you want to know how much longer until 2 pm MST, type in $when 2:00 MST');
+    msg.channel.send('Here is a list of the available timezones:\n'+ printTimeZones());
     //msg.channel.send(`If you want to know how much longer until an event is on January 1st 2025 1 pm MST, type ${prefix}when 01/01/2025 1:00PM MST`);
+  } else if (msg.content.startsWith(`${prefix}`)){
+    msg.channel.send("I'm sorry, I don't understand what you're looking for. Try $help for instructions");
   }
 });
 
-function convertTime2UTC(timeStr) {
-  var time = new Date.now();
-  var now = new Date.now();
+function parseTime( t ) {
+  var d = new Date();
+  var time = t.match( /(\d+)(?::(\d\d))?\s*(P?)/ );
+  d.setHours( parseInt( time[1], 10) + (time[3] ? 12 : 0) );
+  d.setMinutes( parseInt( time[2]) || 0 );
+  return d;
+}
 
-  time = new Date(`${now.year}-${now.month}-${now.day}T`)
-  return now;
+function findTimeZone(s) {
+  var zone = '';
+  for(const[key,value] of Object.entries(timeZones)) {
+    if(s.includes(key)){
+      // pick the longest one that is matched. 
+      if (zone.length < key.length){
+        zone = key;
+        console.log("Found time zone! "+ key);
+      }
+    }
+  }
+  return zone;
+}
+
+function convertTime2UTC(hours,minutes, timeZone) {
+  var now = new Date();
+  var zone = timeZones[timeZone];
+  console.log("is this a timezone obj? " + zone);
+  if (zone === null || zone.offset === null) {
+    return now;
+  }
+  console.log("timezone: " + timeZone.toString());
+  //var offset = timeZones[timeZone].offset;
+  var offset = parseInt(zone.offset);
+  var offsetType = "-";
+  if (offset >= 0)
+    offsetType = '+';
+  offset = Math.abs(offset);
+  offsetMinutes = "00";
+  if (!Number.isInteger(offset)){
+    offset = offset-0.5;
+    offsetMinutes="30";
+  }
+  console.log(`${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}T${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}:00.000${offsetType}${("0"+offset).slice(-2)}:${offsetMinutes}`);
+  var time = new Date(`${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}T${("0" + hours).slice(-2)}:${("0" + minutes).slice(-2)}:00.000${offsetType}${("0"+offset).slice(-2)}:${offsetMinutes}`);
+  console.log(`Going to return ${time}`);
+  if((time.getTime() - now.getTime()) < 0){
+    time.setDate(now.getDate() + 1);
+  }
+  return time;
 }
 
 client.login(process.env.discordbot);
@@ -92,7 +172,7 @@ function printTimeZones() {
   var timeZoneList = '';
   for (const [key, value] of Object.entries(timeZones)) {
     timeZoneList += `${key}:  ${value.name} UTC ${value.offset}
-    `;
+  `;
   }
 
   return timeZoneList;
